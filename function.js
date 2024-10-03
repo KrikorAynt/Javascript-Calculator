6bfe387e7ea098b349ff3e3e2cda8a00ad9f49a8
KrikorAynt <krikorayntablian@gmail.com>
2024-10-03 16:53:32 -0400

    Initial Commit
    Add html, css and js scripts for calculator

---
+++ function.js
@@ -0,0 +1,61 @@
+let output =0;
+let num1,num2='';
+let opp = '';
+
+function numberSelect(num){
+    if(opp){
+        if(num2 && num2!='0')
+            num2+=num;
+        else
+            num2=num;
+        document.getElementById("output").innerHTML = num1+opp+num2;
+    }
+    else{
+        if(num1 && num1!='0' && num1!=output)
+            num1+=num;
+        else
+            num1=num;
+        document.getElementById("output").innerHTML = num1;
+    }
+    
+}
+function oppSelect(opps){
+    opp = opps 
+    if(num1)
+        document.getElementById("output").innerHTML = num1+opp;
+    if(num2)
+        document.getElementById("output").innerHTML +=num2;
+  
+}
+function clearAll(){
+    num1='';
+    num2='';
+    opp = '';
+    document.getElementById("output").innerHTML = "0";
+}
+function clears(){
+    if(opp){
+        num2='';
+        document.getElementById("output").innerHTML = num1+opp;
+    }
+    else{
+        num1='';
+        opp='';
+        document.getElementById("output").innerHTML = 0;
+    }
+
+}
+function calculate(){
+    if(opp=='+')
+        output = parseFloat(num1)+parseFloat(num2);
+    if(opp=='-')
+        output = parseFloat(num1)-parseFloat(num2);
+    if(opp=='*')
+        output = parseFloat(num1)*parseFloat(num2);
+    if(opp=='/')
+        output = parseFloat(num1)/parseFloat(num2);
+    document.getElementById("output").innerHTML = output;
+    num1=""+ output;
+    num2="";
+    opp=""
+}
