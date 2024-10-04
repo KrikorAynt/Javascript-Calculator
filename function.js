68d72a1ff85ab08073260fb8a86b2e34b925b216
KrikorAynt <krikorayntablian@gmail.com>
2024-10-04 14:16:07 -0400

    Update function.js
    Fixed bugs related to "." not being counted as 0.

--- function.js
+++ function.js
@@ -47,8 +47,10 @@ function clears(){
 
 }
 function calculate(){
-    if(!num2)
+    if(!num2 || num2=='.')
         num2='0';
+    if(!num1 || num1=='.')
+        num1='0';
     if(opp=='+')
         output = parseFloat(num1)+parseFloat(num2);
     if(opp=='-')
